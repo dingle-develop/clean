ff30f384d673301e9825533dcd68b8501c8cb5aa
Sebastian B. Knapp <news@young-workers.de>
2020-08-29 02:23:22 -0400

    Init step cleanup

---
+++ lib/dIngle/Waypoint.pm
@@ -0,0 +1,61 @@
+  package dIngle::Waypoint
+# **************************
+; our $VERSION = '0.02'
+# *********************
+; use strict; use warnings; use utf8
+
+; use dIngle
+
+; use Waypoint __PACKAGE__, 
+  Init => {
+     project => sub
+        { my ($self,$project,$config,$modopt) = (@_,{},{})
+        ; $project->basedir(Cwd::getcwd())
+        ; $project->load_config(%$config)
+        ; $project->load_modules(%$modopt)
+        ; dIngle->project($project)
+        },
+    i18n => sub
+        { my ($self) = @_
+        ; local $_
+
+        ; my $lang = dIngle->config('i18n','use')
+        ; unless($lang)
+            { _logger("error","Oh, bitte eine Sprache in der Konfiguration angeben.")
+            ; $lang = 'en'
+            }
+        ; dIngle::I18N->current_language($lang)
+
+        ; my $tags = dIngle->config("i18n","language")
+        ; dIngle::I18N->_all_languages([map { lc } (ref $tags ? @$tags : ($tags))])
+
+        ; if($dIngle::ALL_LANGUAGES)
+            { $self->setup_handle($_)
+                foreach dIngle::I18N->all_languages
+            }
+          else
+            { $self->setup_handle($lang) 
+            }
+
+        ; _logger("debug","Current Language: $lang")
+        }
+  }
+
+; 1
+
+__END__
+
+=head1 NAME
+
+dIngle::Waypoint
+
+=head1 SYNOPSIS
+
+   dIngle::Waypoint::Init->project($project);
+
+=head1 DESCRIPTION
+
+=head2 Init
+
+
+
