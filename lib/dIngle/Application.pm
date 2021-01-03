1a2cffea6dffabc3d8a167331e0b544f003006a6
Sebastian B. Knapp <news@young-workers.de>
2021-01-03 08:49:40 -0500

    loads everything required for a dIngle Application

---
+++ lib/dIngle/Application.pm
@@ -0,0 +1,33 @@
+  package dIngle::Application
+# ***************************
+; our $VERSION='0.06'
+# *******************
+
+; use Sub::Uplevel ()
+; use Package::Subroutine::Sugar
+
+; use strict; use warnings; use utf8
+
+; use dIngle::Library
+
+; use dIngle
+; use dIngle::Project
+; use dIngle::Waypoint
+
+; 1
+  
+__END__
+
+=head1 NAME
+
+dIngle::Application
+
+=head1 SYNOPSIS
+
+    use dIngle::Application;
+
+=head1 DESCRIPTION
+
+This module encapsulates the informations to run a dingle installation.
+Thatswhy it is used early in almost all dingle scripts.
+
