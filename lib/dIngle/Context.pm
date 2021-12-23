b0a93a0b410e4735138522ca84ccecf1a8a983ba
Sebastian B. Knapp <news@young-workers.de>
2021-12-23 07:27:37 -0500

    context object development

--- lib/dIngle/Context.pm
+++ lib/dIngle/Context.pm
@@ -1,20 +1,36 @@
   package dIngle::Context;
 # ************************
-  our $VERSION='0.01_001';
+  our $VERSION='0.02';
 # ************************
 ; use strict; use warnings; use utf8
 
 ; use Carp()
+; use ReleaseAction ()
 ; use subs 'init'
 
 ; use HO::class
-    _rw => container => '$',
+    _ro => hive => '$',
     _rw => object => '$',
-    _ro => stash => '%',
+    _rw => stash => '%',
+    _ro => backend => sub { 'generic' },
     init => 'hash'
     
-; sub take { local $Carp::CarpLevel += 1; shift->container->take(@_) }
+; sub take 
+    { my ($context, $task, @args) = @_
+    ; local $Carp::CarpLevel += 1
+    ; return $context->hive->take($context, $task, @args) 
+    }
     
+; sub set_backend
+    { my $self = $_[0]
+    ; my $current = $self->backend
+    ; $self->[&_backend] = $_[1]
+    ; if(@_ == 3)
+        { $_[2] = ReleaseAction->new(sub { $self->[&_backend] = $current })
+        }
+    ; $self
+    }
+
 ; 1
 
 __END__
