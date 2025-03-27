c68b5eec7433153e15cb81fa2c0b7cf9d19f03e2
Polis Subdance <treepolis@ecobytes.net>
2025-03-27 17:31:22 +0100

    copyright notice

--- lib/dIngle.pm
+++ lib/dIngle.pm
@@ -63,3 +63,18 @@ dIngle - builds things
 
 =head1 DESCRIPTION
 
+=head1 AUTHORS
+
+Sebastian Knapp E<lt> sknpp@cpan.org E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+ 
+This software is copyright (c) 2023 by dingle-developers.
+ 
+This is free software; you can redistribute it and/or modify it under
+the GNU GENERAL PUBLIC LICENSE Version 3.
+ 
+=cut
+
+
+
